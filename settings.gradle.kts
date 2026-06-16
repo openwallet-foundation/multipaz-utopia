091426bc775e9f6d886c1110f0610b84956550fe
Peter Sorotokin <sorotokin@gmail.com>
2026-06-15 17:17:48 -0700

    Move this repo to work with multipaz snapshots.
    Signed-off-by: Peter Sorotokin <sorotokin@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,15 +18,12 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositories {
-        mavenLocal()
-        google {
-            mavenContent {
-                includeGroupAndSubgroups("androidx")
-                includeGroupAndSubgroups("com.android")
-                includeGroupAndSubgroups("com.google")
-            }
-        }
+        google()
         mavenCentral()
+        maven {
+            url = uri("https://central.sonatype.com/repository/maven-snapshots/")
+        }
+        mavenLocal()
         maven("https://jitpack.io") {
             mavenContent {
                 includeGroup("com.github.yuriy-budiyev")
